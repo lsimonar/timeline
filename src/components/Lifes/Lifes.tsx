67c0008328c3eba2534b7d81171780124eb32a61
lsimonar <lluis.simon.92@gmail.com>
2022-12-26 17:51:29 +0100

    wrong cards style improved

--- src/components/Lifes/Lifes.tsx
+++ src/components/Lifes/Lifes.tsx
@@ -11,21 +11,21 @@ interface SquareProps{
 
 function Square({wrongCard}: SquareProps){
     return (
-    < div style={{transform: 'scale(0.5)'}}>
-    <div className="card">
-              <div id={wrongCard.id} className='card-inner flipped' >
-                <div className="card-front">
-                  <img src={process.env.PUBLIC_URL + wrongCard.img} alt={wrongCard.content}></img>
-                  <p className="card-text">{wrongCard.content}</p>
-                </div>
-                <div className="card-back">
-                  <img src={process.env.PUBLIC_URL + wrongCard.img} alt={wrongCard.content}></img>
-                  <p className= 'card-date wrong'>{`${wrongCard.date.getFullYear()}`}</p>
-                  <p className="card-text">{wrongCard.content}</p>
-                </div>
-              </div>
-            </div>
-            </div>
+    <div style={{transform: 'scale(0.5)'} }>
+      <div className="card">
+        <div id={wrongCard.id} className='card-inner flipped' >
+          <div className="card-front">
+            <img src={process.env.PUBLIC_URL + wrongCard.img} alt={wrongCard.content}></img>
+            <p className="card-text">{wrongCard.content}</p>
+          </div>
+          <div className="card-back">
+            <img src={process.env.PUBLIC_URL + wrongCard.img} alt={wrongCard.content}></img>
+            <p className= 'card-date wrong'>{`${wrongCard.date.getFullYear()}`}</p>
+            <p className="card-text">{wrongCard.content}</p>
+          </div>
+        </div>
+      </div>
+    </div>
     )
 }
 
@@ -34,12 +34,10 @@ function Lifes({wrongCards, lifes}: LifesProps) {
     const generateHearts = () => {
         let hearts: JSX.Element[] = [];
         for(let i=0; i< lifes; i++){
-            if (wrongCards[i]){
-            hearts.push(<Square key = {i} wrongCard={wrongCards[i]}/>)
-            }
-            else{
-            hearts.push(<div className= "square"></div>)
-            }
+            hearts.push(
+              <div key = {i} className="square">
+                {wrongCards[i]? <Square wrongCard={wrongCards[i]}/> : ''}
+              </div>)
         }
         return hearts;
     }
