6f8195548eba0de62a6c7175f3e3feb477957e7a
lsimonar <lluis.simon.92@gmail.com>
2023-01-03 18:17:29 +0100

    attempt to keep images loaded

--- src/components/Card/TimelineCard.tsx
+++ src/components/Card/TimelineCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useMemo} from "react";
 import { Draggable } from "react-beautiful-dnd";
 import { Card } from "../../utils/types";
 import './TimelineCard.scss';
@@ -13,6 +13,13 @@ interface cardProps {
 
 export default function TimelineCard({ card, index, isDragDisabled, cardToFlip, isWrong }: cardProps) {
 
+    const imageUrl = process.env.PUBLIC_URL + card.img;
+    const image = useMemo(() => {
+      const img = new Image();
+      img.src = imageUrl;
+      return img;
+    }, [imageUrl]);
+
     return (
       <Draggable isDragDisabled={isDragDisabled} draggableId={card.id} index={index} >
         {(provided) => { 
@@ -26,11 +33,11 @@ export default function TimelineCard({ card, index, isDragDisabled, cardToFlip, 
             <div className="card">
               <div id={card.id} className={`card-inner ${isWrong || cardToFlip === card.id? 'flipped' : ''}`} >
                 <div className="card-front">
-                  <img src={process.env.PUBLIC_URL + card.img} alt={card.content}></img>
+                  <img src={image.src} alt={card.content}></img>
                   <p className="card-text">{card.content}</p>
                 </div>
                 <div className="card-back">
-                  <img src={process.env.PUBLIC_URL + card.img} alt={card.content}></img>
+                  <img src={image.src} alt={card.content}></img>
                   <p className= {isWrong === true ? 'card-date wrong': 'card-date' }>{`${card.date}`}</p>
                   <p className="card-text">{card.content}</p>
                 </div>
