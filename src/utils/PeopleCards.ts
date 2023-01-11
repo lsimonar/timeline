276021e66d8310a4a42e6b43ee718992548bd4e8
lsimonar <lluis.simon.92@gmail.com>
2023-01-11 22:02:31 +0100

    added 80 people cards

--- src/utils/PeopleCards.ts
+++ src/utils/PeopleCards.ts
@@ -110,4 +110,542 @@ export const peopleCards: Card[] = [
   content: "Sigmund Freud's birthday",
   img: ''
   },
+
+  { id: "19",
+  date: -4,
+  content: "Jesus Christ's birthday",
+  img: ''
+  },
+
+  { id: "20",
+  date: 1769,
+  content: "Napoleon Bonaparte's birthday",
+  img: ''
+  },
+
+  { id: "21",
+  date: -1507,
+  content: "Hatshepsut's birthday",
+  img: ''
+  },
+
+  { id: "22",
+  date: -1401,
+  content: "Amenhotep III birthday",
+  img: ''
+  },
+
+  { id: "23",
+  date: -356,
+  content: "Alexander the Great's birthday",
+  img: ''
+  },
+
+  { id: "24",
+  date: -304,
+  content: "Ashoka the Great's birthday",
+  img: ''
+  },
+
+  { id: "25",
+  date: -100,
+  content: "Julius Caesar's birthday",
+  img: ''
+  },
+
+  { id: "26",
+  date: -63,
+  content: "Caesar Augustus' birthday",
+  img: ''
+  },
+
+  { id: "27",
+  date: -69,
+  content: "Cleopatra's birthday",
+  img: ''
+  },
+  
+  { id: "28",
+  date: 30,
+  content: "Boudica's birthday",
+  img: ''
+  },
+
+  { id: "28",
+  date: 624,
+  content: "Wu Zetian's birthday",
+  img: ''
+  },
+
+  { id: "29",
+  date: 849,
+  content: "Alfred the Great's birthday",
+  img: ''
+  },
+
+  { id: "30",
+  date: 1022,
+  content: "Harold II's birthday",
+  img: ''
+  },
+
+  { id: "31",
+  date: 1102,
+  content: "Empress Matilda's birthday",
+  img: ''
+  },
+
+  { id: "32",
+  date: 1122,
+  content: "Eleanor of Aquitaine's birthday",
+  img: ''
+  },
+
+  { id: "33",
+  date: 1133,
+  content: "Henry II's birthday",
+  img: ''
+  },
+
+  { id: "34",
+  date: 1162,
+  content: "Genghis Khan's birthday",
+  img: ''
+  },
+
+  { id: "35",
+  date: 1239,
+  content: "Edward I's birthday",
+  img: ''
+  },
+
+  { id: "36",
+  date: 1280,
+  content: "Mansa Musa's birthday",
+  img: ''
+  },
+
+  { id: "37",
+  date: 1295,
+  content: "Isabella of France's birthday",
+  img: ''
+  },
+
+  { id: "38",
+  date: 1412,
+  content: "Joan of Arc's birthday",
+  img: ''
+  },
+
+  { id: "39",
+  date: 1451,
+  content: "Christopher Columbus' birthday",
+  img: ''
+  },
+
+  { id: "40",
+  date: 1451,
+  content: "Isabella I of Castile's birthday",
+  img: ''
+  },
+
+  { id: "41",
+  date: 1533,
+  content: "Elizabeth I's birthday",
+  img: ''
+  },
+
+  { id: "42",
+  date: 1540,
+  content: "Francis Drake's birthday",
+  img: ''
+  },
+
+  { id: "43",
+  date: 1542,
+  content: "Mary, Queen of Scots' birthday",
+  img: ''
+  },
+
+  { id: "44",
+  date: 1564,
+  content: "William Shakespeare's birthday",
+  img: ''
+  },
+
+  { id: "45",
+  date: 1638,
+  content: "Louis XIV's birthday",
+  img: ''
+  },
+
+  { id: "46",
+  date: 1643,
+  content: "Isaac Newton's birthday",
+  img: ''
+  },
+
+  { id: "47",
+  date: 1729,
+  content: "Catherine the Great's birthday",
+  img: ''
+  },
+
+  { id: "48",
+  date: 1732,
+  content: "George Washington's birthday",
+  img: ''
+  },
+
+  { id: "49",
+  date: 1743,
+  content: "Thomas Jefferson's birthday",
+  img: ''
+  },
+
+  { id: "50",
+  date: 1755,
+  content: "Marie Antoinette's birthday",
+  img: ''
+  },
+
+  { id: "51",
+  date: 1755,
+  content: "Alexander Hamilton's birthday",
+  img: ''
+  },
+
+  { id: "52",
+  date: 1755,
+  content: "Alexander Hamilton's birthday",
+  img: ''
+  },
+
+  { id: "53",
+  date: 1812,
+  content: "Charles Dickens' birthday",
+  img: ''
+  },
+
+  { id: "54",
+  date: 1818,
+  content: "Karl Marx's birthday",
+  img: ''
+  },
+
+  { id: "55",
+  date: 1819,
+  content: "Queen Victoria's birthday",
+  img: ''
+  },
+
+  { id: "56",
+  date: 1869,
+  content: "Mahatma Gandhi's birthday",
+  img: ''
+  },
+
+  { id: "57",
+  date: 1878,
+  content: "Josef Stalin's birthday",
+  img: ''
+  },
+
+  { id: "58",
+  date: 1878,
+  content: "Josef Stalin's birthday",
+  img: ''
+  },
+
+  { id: "59",
+  date: 1913,
+  content: "Rosa Parks' birthday",
+  img: ''
+  },
+
+  { id: "60",
+  date: 1918,
+  content: "Nelson Mandela's birthday",
+  img: ''
+  },
+
+  { id: "61",
+  date: 1929,
+  content: "Martin Luther King's birthday",
+  img: ''
+  },
+
+  { id: "62",
+  date: 1929,
+  content: "Anne Frank's birthday",
+  img: ''
+  },
+
+  { id: "63",
+  date: 1942,
+  content: "Stephen Hawking's birthday",
+  img: ''
+  },
+
+  { id: "64",
+  date: 1883,
+  content: "Coco Chanel's birthday",
+  img: ''
+  },
+
+  { id: "65",
+  date: 1926,
+  content: "Elizabeth II's birthday",
+  img: ''
+  },
+
+  { id: "66",
+  date: 1958,
+  content: "Michael Jackson's birthday",
+  img: ''
+  },
+
+  { id: "67",
+  date: 1926,
+  content: "Marilyn Monroe's birthday",
+  img: ''
+  },
+
+  { id: "68",
+  date: 1961,
+  content: "Barack Obama's birthday",
+  img: ''
+  },
+
+  { id: "69",
+  date: 1756,
+  content: "Wolfgang Amadeus Mozart's birthday",
+  img: ''
+  },
+
+  { id: "70",
+  date: 1961,
+  content: "Diana Spencer's birthday",
+  img: ''
+  },
+
+  { id: "71",
+  date: 1965,
+  content: "J.K.Rowling's birthday",
+  img: ''
+  },
+
+  { id: "72",
+  date: 2003,
+  content: "Greta Thunberg's birthday",
+  img: ''
+  },
+
+  { id: "73",
+  date: -423,
+  content: "Plato's birthday",
+  img: ''
+  },
+
+  { id: "73",
+  date: 1958,
+  content: "Madonna's birthday",
+  img: ''
+  },
+
+  { id: "74",
+  date: 1987,
+  content: "Lionel Messi's birthday",
+  img: ''
+  },
+
+  { id: "75",
+  date: 1981,
+  content: "Roger Federer's birthday",
+  img: ''
+  },
+
+  { id: "76",
+  date: 1986,
+  content: "Usain Bolt's birthday",
+  img: ''
+  },
+
+  { id: "77",
+  date: 1925,
+  content: "Margaret Thatcher's birthday",
+  img: ''
+  },
+
+  { id: "77",
+  date: 1985,
+  content: "Cristiano Ronaldo's birthday",
+  img: ''
+  },
+
+  { id: "78",
+  date: 1977,
+  content: "Shakira's birthday",
+  img: ''
+  },
+
+  { id: "79",
+  date: 1963,
+  content: "Brad Pitt's birthday",
+  img: ''
+  },
+
+  {
+    id: "80",
+    date: 1982,
+    content: "Mark Zuckerberg's birthday",
+    img: ''
+  },
+
+  {
+    id: "81",
+    date: 1984,
+    content: "Ashton Kutcher's birthday",
+    img: ''
+  },
+  
+  {
+    id: "82",
+    date: 1987,
+    content: "Miley Cyrus's birthday",
+    img: ''
+  },
+  
+  {
+    id: "83",
+    date: 1989,
+    content: "Daniel Radcliffe's birthday",
+    img: ''
+  },
+  
+  {
+    id: "84",
+    date: 1992,
+    content: "Zayn Malik's birthday",
+    img: ''
+  },
+  
+  {
+    id: "85",
+    date: 1993,
+    content: "Selena Gomez's birthday",
+    img: ''
+  },
+  
+  {
+    id: "86",
+    date: 1994,
+    content: "Dakota Johnson's birthday",
+    img: ''
+  },
+  
+  {
+    id: "87",
+    date: 1995,
+    content: "Timothee Chalamet's birthday",
+    img: ''
+  },
+
+  {
+    id: "88",
+    date: 1996,
+    content: "Lily-Rose Depp's birthday",
+    img: ''
+  },
+  
+  {
+    id: "89",
+    date: 1997,
+    content: "Millie Bobby Brown's birthday",
+    img: ''
+  },
+
+  {
+    id: "90",
+    date: 1990,
+    content: "Emma Watson's birthday",
+    img: ''
+  },
+  
+  {
+    id: "91",
+    date: 1990,
+    content: "Kendall Jenner's birthday",
+    img: ''
+  },
+  
+  {
+    id: "92",
+    date: 1990,
+    content: "Gigi Hadid's birthday",
+    img: ''
+  },
+  
+  {
+    id: "93",
+    date: 1991,
+    content: "Harry Styles's birthday",
+    img: ''
+  },
+  
+  {
+    id: "94",
+    date: 1991,
+    content: "Rihanna's birthday",
+    img: ''
+  },
+  
+  {
+    id: "95",
+    date: 1992,
+    content: "Bella Hadid's birthday",
+    img: ''
+  },
+  
+  {
+    id: "96",
+    date: 1992,
+    content: "Dua Lipa's birthday",
+    img: ''
+  },
+  
+  {
+    id: "97",
+    date: 1993,
+    content: "Sophie Turner's birthday",
+    img: ''
+  },
+  
+  {
+    id: "98",
+    date: 1993,
+    content: "Joe Jonas's birthday",
+    img: ''
+  },
+  
+  {
+    id: "99",
+    date: 1994,
+    content: "Lena Dunham's birthday",
+    img: ''
+  },
+
+  {
+    id: "100",
+    date: 2001,
+    content: "Billie Eilish's birthday",
+    img: ''
+  },
+
+  {
+    id: "101",
+    date: 1999,
+    content: "Lorde's birthday",
+    img: ''
+  },
 ]
